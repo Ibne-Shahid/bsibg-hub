4e915cf6f326d97d15ac85fef2e445613d65fbf3
Ibne-Shahid <anasbinshahid2004@gmail.com>
2026-03-06 00:14:54 +0600

    env files added

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKEgvqRETpDLPgdO0jheZNAzV_MdokHHE",
-  authDomain: "bsibg-hub.firebaseapp.com",
-  projectId: "bsibg-hub",
-  storageBucket: "bsibg-hub.firebasestorage.app", 
-  messagingSenderId: "418370828737",
-  appId: "1:418370828737:web:fcff0e0a944039d9fa5740",
-  measurementId: "G-3RT5LKCBFG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
